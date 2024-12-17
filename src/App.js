75d745e7ad6eee157732dd2eb95bb2a54cc21e5a
123-anubhav <anubhav.aa2@gmail.com>
2024-12-17 18:40:41 +0530

    App.js Added of useContext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import UseRefHook from './components/UseRefHook';
 import UseReducerHook from './components/UseReducerHook';
+import MessageOne from './components/context-api-using-hooks/MessageOne';
 import './style.css';
+
 
 function App() {
   return (
     <div className="App">
-      <div className="row mt-5">
-        <div className="col-md-1">
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-md-5">
+            <UseRefHook />
+          </div>
+          <div className="col-md-6">
+            <UseReducerHook />
+          </div>
         </div>
-        <div className="col-md-5">
-          <UseRefHook />
-        </div>
-        <div className="col-md-6">
-          <UseReducerHook />
-        </div>
-      </div>
-      <div style={{ marginBottom: '420px' }}>
+        <div className="row mt-3 text-center text-secondary h4">
+          <div className="col">
 
+            context-api-using useContext() Example
+
+            <MessageOne wish="hello i m from parent" />
+          </div>
+        </div>
       </div>
     </div>
+
   );
 }
 
